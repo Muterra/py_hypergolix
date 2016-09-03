51e759880e0c0f4a1873c7936c8e0f737d6b15d6
Nick Badger <badg@nickbadger.com>
2016-09-03 13:22:42 -0700

    Converted daemonize to function

--- hypergolix/_daemonize.py
+++ hypergolix/_daemonize.py
@@ -147,7 +147,7 @@ class Daemonizer:
         self._autoclose_files()
         self._std_redirect()
         
-        # Should this be advanced to just after registering our atexit cleanup?
+        # Should this be advanced to just after forking?
         signal.signal(signal.SIGTERM, self._handle_sigterm)
         
     def _write_pid(self):
@@ -184,6 +184,7 @@ class Daemonizer:
         
         # Skip fd 0, 1, 2, which are used by stdin, stdout, and stderr 
         # (respectively)
+        # NOTE BUG! This ignores shielded fds!
         # How nice of os to include this for us!
         os.closerange(3, fdlimit)
                     
@@ -215,6 +216,8 @@ class Daemonizer:
         # Acquire an exclusive lock. Do not block to acquire it. Failure will 
         # raise an OSError (older versions raised IOError?).
         try:
+            # Note that the flock(2) manpage states the lock will be released
+            # when all open file descriptors of it are closed.
             fcntl.flock(self._locked_pid, fcntl.LOCK_EX | fcntl.LOCK_NB)
             
         except (IOError, OSError):
@@ -364,6 +367,326 @@ class Daemonizer:
         logger.warning('Caught signal. Exiting.')
         sys.exit()
         
+        
+        
+        
+        
+# Daemonization and helpers
+                    
+def _lock_pidfile(pid_file):
+    ''' Gets a lock for the PIDfile. This ensures that only one 
+    instance of this particular daemon is running at any given time.
+    '''
+    # We're going to switch modes depending on if the file exists or not.
+    # Normally I'd open it as append, BUT saw this note in the python docs:
+    # "which on some Unix systems, means that all writes append to the end 
+    # of the file regardless of the current seek position"
+    try:
+        if os.path.isfile(pid_file):
+            logger.warning(
+                'PID file already exists. It will be overwritten with the '
+                'new PID upon successful daemonization.'
+            )
+            locked_pid = open(pid_file, 'r+')
+        else:
+            locked_pid = open(pid_file, 'w+')
+            
+    except (IOError, OSError):
+        logger.critical(
+            'Unable to create/open the PID file w/ traceback: \n' + 
+            ''.join(traceback.format_exc())
+        )
+        sys.exit(1)
+        
+    # Acquire an exclusive lock. Do not block to acquire it. Failure will 
+    # raise an OSError (older versions raised IOError?).
+    try:
+        # Note that the flock(2) manpage states the lock will be released
+        # when all open file descriptors of it are closed.
+        fcntl.flock(locked_pid, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        
+    except (IOError, OSError):
+        logger.critical(
+            'Unable to lock the PID file w/ traceback: \n' + 
+            ''.join(traceback.format_exc())
+        )
+        sys.exit(1)
+        
+    return locked_pid
+        
+def _write_pid(locked_pidfile):
+    ''' Write our PID to the (already locked (by us)) PIDfile.
+    '''
+    locked_pidfile.seek(0)
+    locked_pidfile.truncate(0)
+    pid = str(os.getpid())
+    locked_pidfile.write(pid + '\n')
+    locked_pidfile.flush()
+            
+def _fratricidal_fork():
+    ''' Fork the current process, and immediately exit the parent.
+    
+    OKAY TECHNICALLY THIS WOULD BE PARRICIDE but it just doesn't 
+    have the same ring to it.
+    '''
+    try:
+        # This will create a clone of our process. The clone will get zero
+        # for the PID, and the parent will get an actual PID.
+        pid = os.fork()
+            
+    except OSError as exc:
+        logger.critical(
+            'Fork failed with traceback: \n' + 
+            ''.join(traceback.format_exc())
+        )
+        sys.exit(1)
+    
+    # If PID != 0, this is the parent process, and we should IMMEDIATELY 
+    # die.
+    if pid != 0:
+        # Exit first parent without cleanup.
+        os._exit(0)
+    else:
+        logger.info('Fork successful.')
+        
+def _filial_usurpation(chdir, umask):
+    ''' Decouple the child process from the parent environment.
+    '''
+    # This prevents "directory busy" errors when attempting to remove 
+    # subdirectories.
+    os.chdir(chdir)
+    
+    # Get new PID.
+    # Stop listening to parent signals.
+    # Put process in new parent group
+    # Detatch controlling terminal.
+    new_sid = os.setsid()
+    if new_sid == -1:
+        # A new pid of -1 is bad news bears
+        logger.critical('Failed setsid call.')
+        sys.exit(1)
+        
+    # Set the permissions mask
+    os.umask(umask)
+    
+def _make_range_tuples(start, stop, exclude):
+    ''' Creates a list of tuples for all ranges needed to close all 
+    files between start and stop, except exclude. Ex:
+    start=3, stop=7, exclude={4,}:
+        (3, 4),
+        (5, 7)
+    '''
+    # Make a list copy of exclude, discarding anything less than stop
+    exclude = [ii for ii in exclude if ii >= start]
+    # Sort ascending
+    exclude.sort()
+    
+    ranges = []
+    seeker = start
+    for ii in exclude:
+        this_range = (seeker, ii)
+        ranges.append(this_range)
+        seeker = ii + 1
+    # Don't forget to add the final range!
+    final_range = (seeker, stop)
+    ranges.append(final_range)
+        
+    return ranges
+        
+def _autoclose_files(shielded, fallback_limit=1024):
+    ''' Automatically close any open file descriptors.
+    '''
+    # Figure out the maximum number of files to try to close.
+    # This returns a tuple of softlimit, hardlimit; the hardlimit is always
+    # greater.
+    softlimit, hardlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    
+    # If the hard limit is infinity, we can't iterate to it.
+    if hardlimit == resource.RLIM_INFINITY:
+        # Check the soft limit. If it's also infinity, fallback to guess.
+        if softlimit == resource.RLIM_INFINITY:
+            fdlimit = fallback_limit
+            
+        # The soft limit is finite, so fallback to that.
+        else:
+            fdlimit = softlimit
+            
+    # The hard limit is not infinity, so prefer it.
+    else:
+        fdlimit = hardlimit
+    
+    # Skip fd 0, 1, 2, which are used by stdin, stdout, and stderr 
+    # (respectively)
+    ranges_to_close = _make_range_tuples(
+        start = 3,
+        stop = fdlimit,
+        exclude = shielded
+    )
+    for start, stop in ranges_to_close:
+        # How nice of os to include this for us!
+        os.closerange(start, stop)
+        
+def _flush_stds():
+    ''' Flush stdout and stderr.
+    '''
+    try:
+        sys.stdout.flush()
+    except BlockingIOError:
+        logger.error(
+            'Failed to flush stdout w/ traceback: \n' + 
+            ''.join(traceback.format_exc())
+        )
+        # Honestly not sure if we should exit here.
+    
+    try:
+        sys.stderr.flush()
+    except BlockingIOError:
+        logger.error(
+            'Failed to flush stderr w/ traceback: \n' + 
+            ''.join(traceback.format_exc())
+        )
+        # Honestly not sure if we should exit here.
+        
+def _redirect_stds(stdin_goto, stdout_goto, stderr_goto):
+    ''' Set stdin, stdout, sterr
+    '''
+    # The general strategy here is to:
+    # 1. figure out which unique paths we need to open for the redirects
+    # 2. figure out the minimum access we need to open them with
+    # 3. open the files to get them a file descriptor
+    # 4. copy those file descriptors into the FD's used for stdio, etc
+    # 5. close the original file descriptors
+    
+    # Remove repeated values through a set.
+    streams = {stdin_goto, stdout_goto, stderr_goto}
+    # Transform that into a dictionary of {location: 0, location: 0...}
+    # Basically, start from zero permissions
+    streams = {stream: 0 for stream in streams}
+    # And now create a bitmask for each of reading and writing
+    read_mask = 0b01
+    write_mask = 0b10
+    rw_mask = 0b11
+    # Update the streams dict depending on what access each stream requires
+    streams[stdin_goto] |= read_mask
+    streams[stdout_goto] |= write_mask
+    streams[stderr_goto] |= write_mask
+    # Now create a lookup to transform our masks into file access levels
+    access_lookup = {
+        read_mask: os.O_RDONLY,
+        write_mask = os.O_WRONLY,
+        rw_mask = os.O_RDWR
+    }
+    
+    # Now, use our mask lookup to translate into actual file descriptors
+    for stream in streams:
+        # Transform the mask into the actual access level.
+        access = access_lookup[streams[stream]]
+        # Open the file with that level of access.
+        stream_fd = os.open(stream, access)
+        # And update streams to be that, instead of the access mask.
+        streams[stream] = stream_fd
+        # We cannot immediately close the stream, because we'll get an 
+        # error about a bad file descriptor.
+    
+    # Okay, duplicate our streams into the FDs for stdin, stdout, stderr.
+    stdin_fd = streams[stdin_goto]
+    stdout_fd = streams[stdout_goto]
+    stderr_fd = streams[stderr_goto]
+    # Flush before transitioning
+    _flush_stds()
+    # Do iiiitttttt
+    os.dup2(stdin_fd, 0)
+    os.dup2(stdout_fd, 1)
+    os.dup2(stderr_fd, 2)
+    
+    # Finally, close the extra fds.
+    for duped_fd in streams.values():
+        os.close(duped_fd)
+        
+def daemonize(pid_file, chdir=None, stdin_goto=None, stdout_goto=None, 
+              stderr_goto=None, umask=0o027, shielded_fds=None, 
+              fd_fallback_limit=1024):
+    ''' Performs a classic unix double-fork daemonization. Registers all
+    appropriate cleanup functions.
+    
+    fd_check_limit is a fallback value for file descriptor searching 
+    while closing descriptors.
+    
+    umask is the eponymous unix umask. The default value:
+        1. will allow owner to have any permissions.
+        2. will prevent group from having write permission
+        3. will prevent other from having any permission
+    See https://en.wikipedia.org/wiki/Umask
+    '''
+    ####################################################################
+    # Prep the arguments
+    ####################################################################
+    
+    # Convert the pid_file to an abs path
+    pid_file = os.path.abspath(pid_file)
+    
+    # Get the noop stream, in case Python is using something other than 
+    # /dev/null
+    if hasattr(os, "devnull"):
+        devnull = os.devnull
+    else:
+        devnull = "/dev/null"
+        
+    # Convert any unset std streams to go to dev null
+    stdin_goto = _default_to(stdin_goto, devnull)
+    stdout_goto = _default_to(stdout_goto, devnull)
+    stderr_goto = _default_to(stderr_goto, devnull)
+    
+    # Convert chdir to go to current dir, and also to an abs path.
+    chdir = _default_to(chdir, '.')
+    chdir = os.path.abspath(chdir)
+    
+    # And convert shield_fds to a set
+    shielded_fds = _default_to(shielded_fds, set())
+    shielded_fds = set(shielded_fds)
+    
+    ####################################################################
+    # Begin actual daemonization
+    ####################################################################
+    
+    # Get a lock on the PIDfile before forking anything.
+    locked_pidfile = _lock_pidfile(pid_file)
+    # Make sure we don't accidentally autoclose it though.
+    shielded_fds.add(locked_pidfile.fileno())
+    
+    # Define a memoized cleanup function.
+    def cleanup(pid_path=pid_file, pid_lock=locked_pidfile):
+        pid_lock.close()
+        os.remove(pid_path)
+    
+    # Register this as soon as possible in case something goes wrong.
+    atexit.register(cleanup)
+    # Note that because fratricidal fork is calling os._exit(), our parents
+    # will never call cleanup.
+    
+    # Now fork the toplevel parent, killing it.
+    _fratricidal_fork()
+    # We're now running from within the child. We need to detach ourself 
+    # from the parent environment.
+    _filial_usurpation(chdir, umask)
+    # Okay, re-fork (no zombies!) and continue business as usual
+    _fratricidal_fork()
+    
+    # Do some important housekeeping
+    _write_pid(locked_pidfile)
+    _autoclose_files(shielded_fds, fd_fallback_limit)
+    _redirect_stds(stdin_goto, stdout_goto, stderr_goto)
+    
+    def handle_sigterm(signum, frame):
+        ''' Call sys.exit when a sigterm is received. Or don't! Who 
+        knows!
+        '''
+        logger.warning('Caught signal. Exiting.')
+        sys.exit()
+        
+    # Should this be advanced to just after forking?
+    signal.signal(signal.SIGTERM, self._handle_sigterm)
+       
        
 # Daemotion and helpers
         
